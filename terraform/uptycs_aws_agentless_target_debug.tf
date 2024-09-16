a25935877ec23524b04c387dd19744afb1ef5fb5
Giri Kondisetty <67404190+gkondisetty-uptycs@users.noreply.github.com>
2024-09-16 16:08:29 -0400

    Update uptycs_aws_agentless_target_debug.tf

--- terraform/uptycs_aws_agentless_target_debug.tf
+++ terraform/uptycs_aws_agentless_target_debug.tf
@@ -1,13 +1,14 @@
 locals {
-  integrationPrefix  = "girionpremtf-debug"
-  scannerAccountId   = "794888992839"
-  additionalTags     = {}
-}  // Update above values as required
+  uptycs_aws_agentless_target = {
+    integrationPrefix  = "girionpremtf"
+    scannerAccountId   = "794888992839"
+  }  // Update above values as required
+}
 
 module "uptycs_aws_agentless_target" {
   source = "https://uptycs-terraform-dev.s3.amazonaws.com/terraform-aws-uptycs.zip//modules/agentless_integration/target"
 
-  integration_prefix = local.integrationPrefix
-  scanner_account_id = local.scannerAccountId
-  additional_tags    = local.additionalTags
+  integration_prefix = local.uptycs_aws_agentless_target.integrationPrefix
+  scanner_account_id = local.uptycs_aws_agentless_target.scannerAccountId
+  additional_tags    = local.uptycs_aws_agentless_target.additionalTags
 }
